dcffafad03af96be551b4aeade54c3838cea8aa7
dazhengtianxia0 <52726772+dazhengtianxia0@users.noreply.github.com>
2020-05-26 20:49:12 +0800

    后台管理

--- CategoryApp/admin.py
+++ CategoryApp/admin.py
@@ -1,11 +1,11 @@
+# Register your models here.
+
 from django.contrib import admin
 from .models import Category
 
-
-# from .models import category
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "number")
     list_filter = ("number",)
 
-# Register your models here.
+
